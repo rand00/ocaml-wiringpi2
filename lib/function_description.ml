5a1edc0b12b3ac511eaf2b6ac7f9be836ba0cd9f
rand00 <oth.rand@gmail.com>
2025-02-17 19:24:43 +0100

    Added all the WiringPi bindings I think I need for now, including SPI and I2C - compiles!

--- lib/function_description.ml
+++ lib/function_description.ml
@@ -8,9 +8,54 @@ module Types = Types_generated
 module Functions (F : Ctypes.FOREIGN) = struct
   open F
 
+  (*goto remove*)
   (* let foo_init = foreign "foo_init" (void @-> returning int) *)
-
   (* let foo_fnubar = foreign "foo_fnubar" (string_opt @-> returning int) *)
-
   (* let foo_exit = foreign "foo_exit" (void @-> returning void) *)
+
+  (* see /usr/include/{wiringPi.h | wiringPiSPI.h | wiringPiI2C.h}*)
+
+  module WiringPi = struct
+
+    let setup = foreign "wiringPiSetup" (void @-> returning int)
+    let setupSys = foreign "wiringPiSetupSys" (void @-> returning int)
+    let setupGpio = foreign "wiringPiSetupGpio" (void @-> returning int)
+    let setupPhys = foreign "wiringPiSetupPhys" (void @-> returning int)
+
+    let pinMode = foreign "pinMode" (int @-> int @-> returning void)
+    let pullUpDnControl = foreign "pullUpDnControl" (int @-> int @-> returning void)
+
+    let digitalRead = foreign "digitalRead" (int @-> returning int)
+    let digitalWrite = foreign "digitalWrite" (int @-> int @-> returning void)
+    let pwmWrite = foreign "pwmWrite" (int @-> int @-> returning void)
+    (*goto add more pwm-related bindings if needed*)
+
+    let delay = foreign "delay" (uint @-> returning void)
+    let delayMicroseconds = foreign "delayMicroseconds" (uint @-> returning void)
+    
+  end
+
+  module WiringPiSPI = struct
+
+    let setup = foreign "wiringPiSPISetup" (int @-> int @-> returning int)
+    let setupMode = foreign "wiringPiSPISetupMode" (int @-> int @-> int @-> returning int)
+    let close = foreign "wiringPiSPIClose" (int @-> returning int)
+
+    let dataRW = foreign "wiringPiSPIDataRW" (int @-> ptr uchar @-> int @-> returning int)
+
+    (*Note there is some newer interface too - dunno what 'number' is there..*)
+
+  end
+      
+  module WiringPiI2C = struct
+
+    let setup = foreign "wiringPiI2CSetup" (int @-> returning int)
+
+    let read = foreign "wiringPiI2CRead" (int @-> returning int)
+    let write = foreign "wiringPiI2CWrite" (int @-> int @-> returning int)
+
+    (*goto add more procs if needed*)
+
+  end
+  
 end
--- lib/type_description.ml
+++ lib/type_description.ml
@@ -3,5 +3,44 @@ open Ctypes
 module Types (F : Ctypes.TYPE) = struct
   open F
 
-  (* let foo_version = constant "FOO_VERSION" int *)
+  (* see /usr/include/wiringPi.h*)
+
+  (* Pin modes *)
+
+  let c_INPUT = constant "INPUT" int
+  let c_OUTPUT = constant "OUTPUT" int
+  let c_PWM_OUTPUT = constant "PWM_OUTPUT" int
+  let c_PWM_BAL_OUTPUT = constant "PWM_BAL_OUTPUT" int
+  let c_GPIO_CLOCK = constant "GPIO_CLOCK" int
+  let c_SOFT_PWM_OUTPUT = constant "SOFT_PWM_OUTPUT" int
+  let c_SOFT_TONE_OUTPUT = constant "SOFT_TONE_OUTPUT" int
+  let c_PWM_TONE_OUTPUT = constant "PWM_TONE_OUTPUT" int
+  let c_PM_OFF = constant "PM_OFF" int
+    
+  let c_LOW = constant "LOW" int
+  let c_HIGH = constant "HIGH" int
+
+  (* Pull up/down/none *)
+      
+  let c_PUD_OFF = constant "PUD_OFF" int
+  let c_PUD_DOWN = constant "PUD_DOWN" int
+  let c_PUD_UP = constant "PUD_UP" int
+
+  (* PWM *)
+
+  let c_PWM_MODE_MS = constant "PWM_MODE_MS" int
+  let c_PWM_MODE_BAL = constant "PWM_MODE_BAL" int
+
+  (* Interrupt levels *)
+
+  let c_INT_EDGE_SETUP = constant "INT_EDGE_SETUP" int
+  let c_INT_EDGE_FALLING = constant "INT_EDGE_FALLING" int
+  let c_INT_EDGE_RISING = constant "INT_EDGE_RISING" int
+  let c_INT_EDGE_BOTH = constant "INT_EDGE_BOTH" int
+
+  (* Failure modes *)
+      
+  let c_WPI_FATAL = constant "WPI_FATAL" bool
+  let c_WPI_ALMOST = constant "WPI_ALMOST" bool
+  
 end
